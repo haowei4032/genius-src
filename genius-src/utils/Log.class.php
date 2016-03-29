b3eed782ff21c51948ac3ad80887e5e98e82d6ae
haowei4032 <haowei4032@126.com>
2016-03-29 17:22:54 +0800

    Add Log class

---
+++ genius-src/utils/Log.class.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Genius\Utils;
+
+abstract class Log
+{
+    private function __construct()
+    {}
+
+    public static function d()
+    {}
+
+    public static function i()
+    {}
+
+    public static function e()
+    {}
+
+    public static function w()
+    {}
+
+}

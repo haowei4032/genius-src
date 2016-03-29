ca3629c2e25b762e0a2a90838b72747c0606b617
haowei4032 <haowei4032@126.com>
2016-03-29 17:55:32 +0800

    Rebuild genius kernel

--- test/controllers/index.class.php
+++ test/controllers/index.class.php
@@ -1,22 +1,18 @@
 <?php
 
-/**
- * User: Howay
- * Date: 2015/12/5 0005
- * Time: 12:29
- */
-
-
 namespace Controllers;
 
 use Genius;
 use Genius\Controller\General as Controller;
 
+
 class Index extends Controller
 {
-    public function index()
+
+    public function Index()
     {
-        return Genius::getAlias('@runtime');
+
     }
+
 
 }

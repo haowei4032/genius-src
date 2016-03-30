e06a0475e5f05f0787ef0be36e58c0e7d88e308b
haowei4032 <haowei4032@126.com>
2016-03-30 11:58:15 +0800

    Add directory class

---
+++ vendor/utils/Directory.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @link http://genius.haowei.me
+ * @copyright Copyright (c) 2015 Genius Software LLC
+ * @license http://genius.haowei.me/license
+ */
+
+namespace Genius\Utils;
+
+abstract class Directory
+{
+    private function __construct()
+    {}
+
+    /**
+     * @param string $path
+     * @return string
+     */
+    public static function create($path)
+    {
+        if(!$path) return null;
+        if(!is_dir($path)) {
+            if(!mkdir($path, 755)) {
+                static::createIt(dirname($path));
+            }
+        }
+        return $path;
+    }
+
+    public static function delete()
+    {}
+}

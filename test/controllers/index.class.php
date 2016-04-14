564ff8033c240a31e7827f353441ed6ce5381800
haowei4032 <haowei4032@126.com>
2016-04-14 19:44:24 +0800

    Test code

--- test/controllers/index.class.php
+++ test/controllers/index.class.php
@@ -13,8 +13,19 @@ use Genius\Controller\General as Controller;
 
 class Index extends Controller
 {
+    public function Index()
+    {
+        var_dump($this);
+    }
+
+    public function Output($id = 5)
+    {
+        var_dump($id);
+    }
+
     public function __before()
     {
-        //$this->redirect('*', 'm.mwee.cn', [ 'id' => 1, 'forward' => '$.url' ] );
+        echo 1;
     }
+
 }

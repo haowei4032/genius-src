b04177348699d75533b268f726cca3d431a7092e
haowei4032 <haowei4032@126.com>
2016-05-06 21:28:19 +0800

    Optimization genius kernel

---
+++ vendor/database/ActiveRecord.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @link http://genius.haowei.me
+ * @copyright Copyright (c) 2015 Genius Software LLC
+ * @license http://genius.haowei.me/license
+ */
+
+namespace Genius\Database;
+
+abstract class ActiveRecord {
+
+    public static function tableName()
+    {}
+
+}
